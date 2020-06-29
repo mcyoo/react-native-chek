17bfbd4c8c8819bd33ba8317b115c7086fd3c1e0
mcyoo <64882430+mcyoo@users.noreply.github.com>
2020-06-29 16:09:23 +0900

    chek app json receive '하이'

---
+++ listviews.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Text, StyleSheet, SectionList} from 'react-native';
+
+export default function listviews({user_json_data}) {
+  return (
+    <View style={styles.container}>
+      {user_json_data.map(item => (
+        <Text>{item.title}</Text>
+      ))}
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
